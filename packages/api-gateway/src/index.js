d33a778483ac5d8926991633d6300e2dd5ca9e16
jacorreamed <jacorreamed@gmail.com>
2022-05-19 19:26:16 -0500

    fix: add .env

--- packages/api-gateway/src/index.js
+++ packages/api-gateway/src/index.js
@@ -3,3 +3,5 @@ const HttpServer = require('./drivers/http/server');
 
 // eslint-disable-next-line no-console
 HttpServer.start().catch((err) => console.error('Something went wrong running server', err));
+
+
